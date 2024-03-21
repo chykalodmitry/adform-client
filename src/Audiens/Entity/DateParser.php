4e63fd8ce0ddb256e29e432cae23fe051c5ec72b
Dmytro Chykalo <chykalodmitry@gmail.com>
2024-03-21 16:44:38 +0100

    - patter fix

--- src/Audiens/Entity/DateParser.php
+++ src/Audiens/Entity/DateParser.php
@@ -20,9 +20,9 @@ class DateParser
         }
 
         $format = 'Y-m-d\TH:i:sP'; // ISO 8601
-        if (\preg_match('`^\d{4}-\d{2}-\d{2}$`', $date)) {
+        if (\preg_match('/^\d{4}-\d{2}-\d{2}$/', $date)) {
             $date .= 'T00:00:00+00:00';
-        } elseif (\preg_match('`^(\d{4}-\d{2}-\d{2}\T\d{2}:\d{2}:\d{2})(\.\d{1,3})?Z$`', $date, $matches)) {
+        } elseif (\preg_match('/^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2})(\.\d{1,3})?Z$/', $date, $matches)) {
             $date = $matches[1].'+00:00';
         }
 
